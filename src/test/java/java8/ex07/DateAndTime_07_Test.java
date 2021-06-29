81c0101547f0400e81649f08dcb3c237d1b6b05f
Melheb younes <melheb.younes@gmail.com>
2021-06-29 13:48:03 +0200

    tp date-and-time

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -2,9 +2,8 @@ package java8.ex07;
 
 import org.junit.Test;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +18,14 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date(117, Calendar.FEBRUARY,12);
 
         // TODO transformer la date en Instant
-
+        Instant dateInstance = date.toInstant();
         // TODO transformer la date en LocalDate
+        ZoneId zoneId = ZoneId.systemDefault();
 
-        LocalDate result = null;
+        LocalDate result = LocalDate.ofInstant(dateInstance,zoneId);
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
